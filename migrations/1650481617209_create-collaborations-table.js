3c9d15ded9eee838370e9ab4867ad055a96ef765
Faiz Murtadha <faizmrtdha99@gmail.com>
2022-04-28 18:13:50 +0700

    local storage implemented

--- migrations/1650481617209_create-collaborations-table.js
+++ migrations/1650481617209_create-collaborations-table.js
@@ -1,18 +1,20 @@
 /* eslint-disable camelcase */
 
+exports.shorthands = undefined;
+
 exports.up = (pgm) => {
   // membuat table collaborations
-  pgm.createTable("collaborations", {
+  pgm.createTable('collaborations', {
     id: {
-      type: "VARCHAR(50)",
+      type: 'VARCHAR(50)',
       primaryKey: true,
     },
     note_id: {
-      type: "VARCHAR(50)",
+      type: 'VARCHAR(50)',
       notNull: true,
     },
     user_id: {
-      type: "VARCHAR(50)",
+      type: 'VARCHAR(50)',
       notNull: true,
     },
   });
@@ -21,22 +23,22 @@ exports.up = (pgm) => {
     Menambahkan constraint UNIQUE, kombinasi dari kolom note_id dan user_id.
     Guna menghindari duplikasi data antara nilai keduanya.
   */
-  pgm.addConstraint("collaborations", "unique_note_id_and_user_id", "UNIQUE(note_id, user_id)");
+  pgm.addConstraint('collaborations', 'unique_note_id_and_user_id', 'UNIQUE(note_id, user_id)');
 
   // memberikan constraint foreign key pada kolom note_id dan user_id terhadap notes.id dan users.id
   pgm.addConstraint(
-    "collaborations",
-    "fk_collaborations.note_id_notes.id",
-    "FOREIGN KEY(note_id) REFERENCES notes(id) ON DELETE CASCADE"
+    'collaborations',
+    'fk_collaborations.note_id_notes.id',
+    'FOREIGN KEY(note_id) REFERENCES notes(id) ON DELETE CASCADE',
   );
   pgm.addConstraint(
-    "collaborations",
-    "fk_collaborations.user_id_users.id",
-    "FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE"
+    'collaborations',
+    'fk_collaborations.user_id_users.id',
+    'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE',
   );
 };
 
 exports.down = (pgm) => {
   // menghapus tabel collaborations
-  pgm.dropTable("collaborations");
+  pgm.dropTable('collaborations');
 };
--- src/server.js
+++ src/server.js
@@ -1,42 +1,55 @@
 // mengimpor dotenv dan menjalankan konfigurasinya
-require("dotenv").config();
+require('dotenv').config();
 
-const Hapi = require("@hapi/hapi");
-const Jwt = require("@hapi/jwt");
+const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
+const Inert = require('@hapi/inert');
+const path = require('path');
 
 // notes
-const notes = require("./api/notes");
-const NotesService = require("./services/postgres/NotesService");
-const NotesValidator = require("./validator/notes");
+const notes = require('./api/notes');
+const NotesService = require('./services/postgres/NotesService');
+const NotesValidator = require('./validator/notes');
 
 // users
-const users = require("./api/users");
-const UsersService = require("./services/postgres/UsersService");
-const UsersValidator = require("./validator/users");
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
 
 // authentications
-const authentications = require("./api/authentications");
-const AuthenticationsService = require("./services/postgres/AuthenticationsService");
-const TokenManager = require("./tokenize/TokenManager");
-const AuthenticationsValidator = require("./validator/authentications");
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
 
 // collaborations
-const collaborations = require("./api/collaborations");
-const CollaborationsService = require("./services/postgres/CollaborationsService");
-const CollaborationsValidator = require("./validator/collaborations");
+const collaborations = require('./api/collaborations');
+const CollaborationsService = require('./services/postgres/CollaborationsService');
+const CollaborationsValidator = require('./validator/collaborations');
+
+// Exports
+const _exports = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
+
+// uploads
+const uploads = require('./api/uploads');
+const StorageService = require('./services/storage/StorageService');
+const UploadsValidator = require('./validator/uploads');
 
 const init = async () => {
   const collaborationsService = new CollaborationsService();
   const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
+  const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/images'));
 
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
     routes: {
       cors: {
-        origin: ["*"],
+        origin: ['*'],
       },
     },
   });
@@ -46,10 +59,13 @@ const init = async () => {
     {
       plugin: Jwt,
     },
+    {
+      plugin: Inert,
+    },
   ]);
 
   // mendefinisikan strategy autentikasi jwt
-  server.auth.strategy("notesapp_jwt", "jwt", {
+  server.auth.strategy('notesapp_jwt', 'jwt', {
     keys: process.env.ACCESS_TOKEN_KEY,
     verify: {
       aud: false,
@@ -95,6 +111,20 @@ const init = async () => {
         collaborationsService,
         notesService,
         validator: CollaborationsValidator,
+      },
+    },
+    {
+      plugin: _exports,
+      options: {
+        service: ProducerService,
+        validator: ExportsValidator,
+      },
+    },
+    {
+      plugin: uploads,
+      options: {
+        service: storageService,
+        validator: UploadsValidator,
       },
     },
   ]);
